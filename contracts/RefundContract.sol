df2bde6b048864fb251021ac02bded832a374fbb
Marvin Splitt <marvin.splitt@gmx.net>
2023-02-23 19:52:45 +0100

    Add addDeliveryPartner function

--- contracts/RefundContract.sol
+++ contracts/RefundContract.sol
@@ -3,8 +3,19 @@ pragma solidity ^0.8.17;
 
 contract RefundContract {
     address public owner;
+    // array of delivery partner addresses
+    address[] private deliveryPartners;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not the owner");
+        _;
+    }
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function addDeliveryPartner(address _deliveryPartner) public onlyOwner {
+        deliveryPartners.push(_deliveryPartner);
     }
 }
