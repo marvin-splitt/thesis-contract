ceadeb9c68abea847ecc351b29db6bf5e8002205
Marvin Splitt <marvin.splitt@gmx.net>
2023-02-24 16:28:15 +0100

    Library for unique tx ids

---
+++ contracts/UniqueId.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.17;
+
+library UniqueId {
+    function getUniqueId() public view returns (bytes32) {
+        return keccak256(abi.encode(msg.sender, block.timestamp));
+    }
+}
